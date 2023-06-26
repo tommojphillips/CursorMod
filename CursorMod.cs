670a8556e1900516197f5fbef80d9c37b2b2e7f2
Tommo J. Phillips <tommojphillips@gmail.com>
2023-06-26 18:56:04 +1000

    Add project files.

---
+++ CursorMod.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using HutongGames.PlayMaker;
+using MSCLoader;
+
+namespace TommoJProductions.CursorMod
+{
+    public class CursorMod : Mod
+    {
+        // Started, 06.11.2022
+
+        public override string ID => "CursorMod";
+        public override string Name => "Cursor Toggle Mod";
+        public override string Author => "tommojphillips";
+        public override string Version => VersionInfo.version;
+        public override string Description => "Adds a keybind to toggle the cursor ingame. (default key is mouse scroll wheel button)";
+
+        private Keybind cursorToggleKeybindPrimary;
+        private Keybind cursorToggleKeybindSecondary;
+
+        private FsmBool playerInMenu;
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, onLoad);
+            SetupFunction(Setup.Update, update);
+        }
+
+        private void onLoad()
+        {
+            playerInMenu = PlayMakerGlobals.Instance.Variables.FindFsmBool("PlayerInMenu");
+        }
+
+        private void update()
+        {
+            cursorFunction();
+        }
+
+        public override void ModSettings()
+        {
+            cursorToggleKeybindPrimary = Keybind.Add(this, "CursorToggle1", "Cursor Toggle Primary", KeyCode.Mouse2);
+            cursorToggleKeybindSecondary = Keybind.Add(this, "CursorToggle2", "Cursor Toggle Secondary", KeyCode.None);
+        }
+
+        private void cursorFunction()
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetMouseButtonDown(2))
+            {
+                playerInMenu.Value = !playerInMenu.Value;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("CursorMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tommo J. Productions")]
+[assembly: AssemblyProduct("CursorMod")]
+[assembly: AssemblyCopyright("Tommo J. Productions Copyright © 2022")]
+[assembly: AssemblyTrademark("Azine")]
+[assembly: NeutralResourcesLanguage("en-AU")]
+
+// Version information
+[assembly: AssemblyVersion("1.0.309.5")]
+[assembly: AssemblyFileVersion("1.0.309.5")]
+
+namespace TommoJProductions.CursorMod
+{
+
+    public class VersionInfo
+    {
+	    public const string lastestRelease = "06.11.2022 11:35 AM";
+	    public const string version = "1.0.309.5";
+
+        /// <summary>
+        /// Represents if the mod has been complied for x64
+        /// </summary>
+        #if x64
+            internal const bool IS_64_BIT = true;
+        #else
+            internal const bool IS_64_BIT = false;
+        #endif
+        /// <summary>
+        /// Represents if the mod has been complied in Debug mode
+        /// </summary>
+        #if DEBUG
+            internal const bool IS_DEBUG_CONFIG = true;
+        #else
+            internal const bool IS_DEBUG_CONFIG = false;
+        #endif
+    }
+}
